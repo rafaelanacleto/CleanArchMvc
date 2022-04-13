e698cea955ce73190947cadb0dc28a83a4b73916
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-04-13 09:21:00 -0300

    igualando branchs

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "O campos name não pode ser vazio");
+
+            DomainExceptionValidation.When(name.Length <3,
+               "O campos name não pode ter menos de 3 caracteres.");
+
+            Name = name;
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public ICollection<Product> Products { get; private set; }
+
+    }
+}
