cb4613a5b51bd6e3aa068998f06326613d1aafc8
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-04-29 15:36:51 -0300

    removendo espaço em branco

--- CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -11,7 +11,6 @@ namespace CleanArchMvc.Domain.Interfaces
     {
         Task<IEnumerable<Category>> GetCatories();
         Task<Category> GetById(int? id);
-
         Task<Category> Create(Category category);
         Task<Category> Update(Category category);
         Task<Category> Remove(Category category);
