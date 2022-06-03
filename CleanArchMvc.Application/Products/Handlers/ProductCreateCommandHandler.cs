7acb794d2574cd2e3ee2af8721130ae6309ddb1e
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-06-03 16:56:45 -0300

    yhndler

---
+++ CleanArchMvc.Application/Products/Handlers/ProductCreateCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    public class ProductCreateCommandHandler : IRequestHandler<ProductCreateCommand, Product>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductCreateCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public Task<Product> Handle(ProductCreateCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
