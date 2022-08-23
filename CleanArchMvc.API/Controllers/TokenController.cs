ba886f953d59a0e67d0fbee624640f75116eeb9f
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-08-23 11:42:59 -0300

    Implementando validacao token

--- CleanArchMvc.API/Controllers/TokenController.cs
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -2,6 +2,10 @@
 using CleanArchMvc.Domain.Account;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace CleanArchMvc.API.Controllers
 {
@@ -9,11 +13,12 @@ namespace CleanArchMvc.API.Controllers
     [ApiController]
     public class TokenController : ControllerBase
     {
-
+        private readonly IConfiguration _configuration;
         private readonly IAuthenticate _authentication;
-        public TokenController(IAuthenticate authentication)
+        public TokenController(IAuthenticate authentication, IConfiguration configuration)
         {
             _authentication = authentication;
+            _configuration = configuration;
         }
 
         [HttpPost("LoginUser")]
@@ -23,8 +28,8 @@ namespace CleanArchMvc.API.Controllers
 
             if (result)
             {
-                //return GenerateToken(userInfo);
-                return Ok($"User {userInfo.Email} login successfully");
+                return GenerateToken(userInfo);
+                //return Ok($"User {userInfo.Email} login successfully");
             }
             else
             {
@@ -41,7 +46,7 @@ namespace CleanArchMvc.API.Controllers
 
             if (result)
             {
-                //return GenerateToken(userInfo);
+               // return GenerateToken(userInfo);
                 return Ok($"User {userInfo.Email} was created successfully");
             }
             else
@@ -51,6 +56,46 @@ namespace CleanArchMvc.API.Controllers
             }
         }
 
+        private UserToken GenerateToken(LoginModel userInfo)
+        {
+            //declarações do usuário
+            var claims = new[]
+            {
+                new Claim("email", userInfo.Email),
+                new Claim("meuvalor", "oque voce quiser"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //gerar chave privada para assinar o token
+            var privateKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+            //gerar a assinatura digital
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            //definir o tempo de expiração
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            //gerar o token
+            JwtSecurityToken token = new JwtSecurityToken(
+                //emissor
+                issuer: _configuration["Jwt:Issuer"],
+                //audiencia
+                audience: _configuration["Jwt:Audience"],
+                //claims
+                claims: claims,
+                //data de expiracao
+                expires: expiration,
+                //assinatura digital
+                signingCredentials: credentials
+                );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
 
 
     }
