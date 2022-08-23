f5ad48df88190425cc17c11fcc03c35e9573e759
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-08-23 11:25:04 -0300

    Criando camada de seguranca jwt da api com token bearer

---
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,57 @@
+﻿using CleanArchMvc.API.Models;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+
+        private readonly IAuthenticate _authentication;
+        public TokenController(IAuthenticate authentication)
+        {
+            _authentication = authentication;
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.Authenticate(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                //return GenerateToken(userInfo);
+                return Ok($"User {userInfo.Email} login successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpPost("CreateUser")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<ActionResult> CreateUser([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.RegisterUser(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                //return GenerateToken(userInfo);
+                return Ok($"User {userInfo.Email} was created successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+
+
+
+    }
+}
