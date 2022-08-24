9116773e4b30f17424acad7bbb24ef481847190e
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-08-24 10:09:10 -0300

    Add referencia token

--- CleanArchMvc.Infra.IoC/DependencyInjectionJWT.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjectionJWT.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
