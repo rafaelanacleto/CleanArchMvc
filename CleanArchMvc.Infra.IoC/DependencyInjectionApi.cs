8364d5ef88a979a6458e4ca3c756ee2be2a78b77
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-08-24 18:36:23 -0300

    Refatoração para c# 11

--- CleanArchMvc.Infra.IoC/DependencyInjectionApi.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjectionApi.cs
@@ -1,48 +1,46 @@
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using CleanArchMvc.Application.Services;
-using CleanArchMvc.Application.Interfaces;
-using AutoMapper;
-using CleanArchMvc.Application.Mappings;
 using System;
-using MediatR;
-using CleanArchMvc.Infra.Data.Identity;
-using Microsoft.AspNetCore.Identity;
-using CleanArchMvc.Domain.Account;
 
-namespace CleanArchMvc.Infra.IoC
+namespace CleanArchMvc.Infra.IoC;
+
+public static class DependencyInjectionApi
 {
-    public static class DependencyInjectionApi
+    public static IServiceCollection AddInfrastructureApi(this IServiceCollection services,
+        IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructureApi(this IServiceCollection services,
-            IConfiguration configuration)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
-            ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        services.AddDbContext<ApplicationDbContext>(options =>
+         options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
+        ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-               .AddEntityFrameworkStores<ApplicationDbContext>()
-               .AddDefaultTokenProviders();           
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+           .AddEntityFrameworkStores<ApplicationDbContext>()
+           .AddDefaultTokenProviders();           
 
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<IProductRepository, ProdutctRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProdutctRepository>();
 
-            services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICategoryService, CategoryService>();
 
-            services.AddScoped<IAuthenticate, AuthenticateService>();            
+        services.AddScoped<IAuthenticate, AuthenticateService>();            
 
-            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
-            var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
-            services.AddMediatR(myHandlers);
+        var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+        services.AddMediatR(myHandlers);
 
-            return services;
-        }
+        return services;
     }
 }
