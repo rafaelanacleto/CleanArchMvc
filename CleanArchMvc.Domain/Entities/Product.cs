c8b2c797ed6f3730e3ae51369a3017e5ca24905a
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-05-22 10:42:14 -0300

    Comentado linha de codigo que estava gerando erro

--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -63,7 +63,7 @@ namespace CleanArchMvc.Domain.Entities
 
         public int CategoryId { get; set; }
         public Category Category { get; set; }
-        public string NomeCategoria { get => NomeCategoria; set => NomeCategoria = Category.Name; }
+       // public string NomeCategoria { get => NomeCategoria; set => NomeCategoria = Category.Name; }
 
     }
 }
