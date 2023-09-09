4315adca9d63984ae15e34e5b132b9abed922485
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-09-09 13:31:26 -0300

    criado ValidateDomain para o domain financeiro

--- CleanArchMvc.Domain/Entities/Financeiro.cs
+++ CleanArchMvc.Domain/Entities/Financeiro.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
 
 namespace CleanArchMvc.Domain.Entities
 {
@@ -20,8 +25,42 @@ namespace CleanArchMvc.Domain.Entities
 
 
         // Construtor
-        public Financeiro()
+        public Financeiro(string nome, string tipo, decimal valor, DateTime dataTransacao, string descricao,
+                           string categoria, bool foiPago, int alunoId)
         {
+            ValidateDomain(nome, tipo, valor, dataTransacao, descricao, categoria, foiPago, alunoId);
         }
+
+        private void ValidateDomain(string nome, string tipo, decimal valor, DateTime dataTransacao, string descricao,
+                            string categoria, bool foiPago, int alunoId)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(nome),
+                "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(nome.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(tipo),
+                "Invalid Tipo. Tipo is required");
+
+            DomainExceptionValidation.When(valor < 0,
+                "Invalid Valor, too short, minimum 5 characters");
+
+            DomainExceptionValidation.When(valor < 0, "Invalid valor value");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(descricao), "Invalid Descricao, Descricao is required");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(categoria), "Invalid Categoria, Categoria is required");
+            
+            Nome = nome;
+            Tipo = tipo;
+            Valor = valor;
+            DataTransacao = dataTransacao;
+            Descricao = descricao;
+            Categoria = categoria;
+            FoiPago = foiPago;
+            AlunoId = alunoId;
+
+        }
+
     }
 }
