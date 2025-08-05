102b56f6352537632ef9d05eb338498339f549bb
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-08-05 11:30:57 -0300

    Adiciona arquivos de configuração do projeto e implementa métodos não finalizados no AtivoRepository

--- CleanArchMvc.Infra.Data/Repositories/AtivoRepository.cs
+++ CleanArchMvc.Infra.Data/Repositories/AtivoRepository.cs
@@ -1,73 +1,60 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using CleanArchMvc.Domain.Entities;
 using CleanArchMvc.Domain.Interfaces;
-using CleanArchMvc.Infra.Data.Context;
 
-namespace CleanArchMvc.Infra.Data.Repositories
+namespace CleanArchMvc.Infra.Data.Repositories;
+
+public class AtivoRepository : IAtivoRepository
 {
-    public class AtivoRepository : IAtivoRepository
+    public Task<IEnumerable<Ativo>> GetAtivos()
     {
+        throw new NotImplementedException();
+    }
 
-        private readonly ApplicationDbContext _context;
+    public Task<Ativo> GetById(int? id)
+    {
+        throw new NotImplementedException();
+    }
 
-        public AtivoRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public Task<Ativo> Create(Ativo ativo)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<Ativo> Create(Ativo ativo)
-        {
-            _context.Add(ativo);
-            _context.SaveChangesAsync();
-            return Task.FromResult(ativo);
-        }
+    public Task<Ativo> Update(Ativo ativo)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<IEnumerable<Ativo>> GetAtivos()
-        {
-            return Task.FromResult(_context.Ativos.AsEnumerable());
-        }
+    public Task<Ativo> Remove(Ativo ativo)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<IEnumerable<Ativo>> GetAtivosByCategoryId(int categoryId)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<IEnumerable<Ativo>> GetAtivosByDateRange(DateTime startDate, DateTime endDate)
-        {
-            return Task.FromResult(_context.Ativos.Where(a => a.DataCriacao >= startDate && a.DataCriacao <= endDate).AsEnumerable());
-        }
+    public Task<IEnumerable<Ativo>> GetAtivosByStatus(bool status)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<IEnumerable<Ativo>> GetAtivosByName(string name)
-        {
-            return Task.FromResult(_context.Ativos.Where(a => a.Nome.Contains(name)).AsEnumerable());
-        }
+    public Task<IEnumerable<Ativo>> GetAtivosByDateRange(DateTime startDate, DateTime endDate)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<IEnumerable<Ativo>> GetAtivosByPriceRange(decimal minPrice, decimal maxPrice)
-        {
-            return Task.FromResult(_context.Ativos.Where(a => a.Valor >= minPrice && a.Valor <= maxPrice).AsEnumerable());
-        }
+    public Task<IEnumerable<Ativo>> GetAtivosByPriceRange(decimal minPrice, decimal maxPrice)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<IEnumerable<Ativo>> GetAtivosByStatus(bool status)
-        {
-            return Task.FromResult(_context.Ativos.Where(a => a.IsAtivo == status).AsEnumerable());
-        }
-
-        public Task<Ativo> GetById(int? id)
-        {
-            return Task.FromResult(_context.Ativos.Find(id));
-        }
-
-        public Task<Ativo> Remove(Ativo ativo)
-        {
-            _context.Remove(ativo);
-            _context.SaveChangesAsync();
-            return Task.FromResult(ativo);
-        }
-
-        public Task<Ativo> Update(Ativo ativo)
-        {
-            _context.Update(ativo);
-            _context.SaveChangesAsync();
-            return Task.FromResult(ativo);
-        }
+    public Task<IEnumerable<Ativo>> GetAtivosByName(string name)
+    {
+        throw new NotImplementedException();
     }
 }
