f0803b14d6371454fff0646044c7e9260df42b72
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-12-10 18:47:40 -0300

    Integração inicial com Elasticsearch e modelo User
    Adicionado pacote Elastic.Clients.Elasticsearch ao projeto. Criados arquivos User.cs (modelo de usuário), IElasticService.cs (interface de serviço) e ElasticService.cs (implementação inicial). Iniciada estrutura para operações de indexação, busca, atualização e remoção de usuários no Elasticsearch.

---
+++ CleanArchMvc.API/Services/ElasticService.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchMvc.API.Models;
+
+namespace CleanArchMvc.API.Services
+{
+    public class ElasticService : IElasticService
+    {
+        public Task<bool> AddOrUpdate(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddOrUpdateBulk(IEnumerable<User> users, string indexName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateIndexAsync(string indexName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<long?> DeleteAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CleanArchMvc.API/Services/IElasticService.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchMvc.API.Models;
+
+namespace CleanArchMvc.API.Services
+{
+    public interface IElasticService
+    {
+        // create index
+        Task CreateIndexAsync(string indexName);
+
+        //add or update document
+        Task<bool> AddOrUpdate(User user);
+
+        //add or update documents in bulk
+        Task<bool> AddOrUpdateBulk(IEnumerable<User> users, string indexName);
+
+        //Get user
+        Task<User> GetUserAsync(string key);
+
+        //Get All users
+        Task<IEnumerable<User>> GetAllUsersAsync();
+
+        //Delete user
+        Task<bool> DeleteUserAsync(string key);
+
+        //Delete all
+        Task<long?> DeleteAllAsync();
+
+    }
+}
