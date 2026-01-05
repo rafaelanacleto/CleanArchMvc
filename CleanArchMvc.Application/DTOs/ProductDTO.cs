0d71a612330bd7ee3e1f43c439a87d873e10b6b6
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2026-01-05 09:55:49 -0300

    Refatoração no endpoint getproducts para não trazer produto repetidos

--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -38,8 +38,6 @@ namespace CleanArchMvc.Application.DTOs
         [DisplayName("Product Image")]
         public string Image { get; set; }
         
-        public Category Category { get; set; }
-
         [DisplayName("Categories")]
         public int CategoryId { get; set; }
     }
